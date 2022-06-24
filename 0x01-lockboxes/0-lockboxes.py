972616cabc450b9141982a9d56b247e61c2b770d
Samie <samrasolomon40@gmail.com>
2022-06-24 18:57:43 +0000

    Added file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,25 +6,11 @@ def canUnlockAll(boxes):
     """This function will take a list of lists and the content
        of a list will unlock other lists
     """
-    tests = set()
-    lists = set()
-    # Add all values of boxes whether they are keys or not
+    lists = [0]
     for i in range(len(boxes)):
         for key in boxes[i]:
-            tests.add(key)
-    # Adds into set the first list, which is unlocked
-    for key in boxes[0]:
-        lists.add(key)
-    # Checks if the value of list of list is in the key container
-    # 'lists'
-    for i in range(len(boxes)):
-        for key in boxes[i]:
-            if (key in lists):
-                # If key is in the collection of keys then open
-                # the list of list box and add contents into 'lists'
-                for value in boxes[key]:
-                    lists.add(value)
-    # If all the key in 'lists' aren't in test return False
-    if (tests != lists):
-        return False
-    return True
+            if (key not in lists and key != i):
+                lists.append(key)
+    if len(lists) == len(boxes):
+        return True
+    return False
