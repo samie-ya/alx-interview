c44b293d2ddb0c58f298b642bd700317a1e254b3
Samie <samrasolomon40@gmail.com>
2022-07-13 17:14:03 +0000

    Made byte check modification

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,11 +11,11 @@ def validUTF8(data):
             break
         else:
             num_byte += 1
-    if (num_byte > 1 and num_byte < 5):
+    if (num_byte > 1 and num_byte <= 4):
         for i in range(1, num_byte):
             bits = format(data[i], '#010b')[-8:]
-            if (bits[0] != '1' and bits[1] != '0'):
+            if not (bits[0] == '1' and bits[1] == '0'):
                 return False
-    elif (num_byte >= 5 or num_byte == 1):
+    elif (num_byte > 5 or num_byte == 1):
         return False
     return True
