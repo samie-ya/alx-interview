4415927255b0ee09e0ebc45af323d1f553cb5751
Samie <samrasolomon40@gmail.com>
2022-07-13 14:49:24 +0000

    Removed unneeded bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,24 +4,18 @@
 
 def validUTF8(data):
     """This script will return true if given list is a valid UTF-8"""
-    num_byte = 0
+    num_byte = 1
     bits = format(data[0], '08b')
-    if len(bits) == 8:
-        for bit in bits:
-            if bit[0] == '0':
-                break
-            else:
-                if bit != '0':
-                    num_byte += 1
-                break
-        if (num_byte > 1 and num_byte < 5):
-            for i in range(1, num_byte):
-                bits = format(data[i], '08b')
-                if len(bits) == 8:
-                    if (bits[0] != '1' and bits[1] != '0'):
-                        return False
+    for bit in bits:
+        if bit[0] == '0':
+            break
+        else:
+            if bit != '0':
+                num_byte += 1
+            break
+    if (num_byte > 1 and num_byte < 5):
+        for i in range(1, num_byte):
+            bits = format(data[i], '08b')
+            if (bits[0] != '1' and bits[1] != '0'):
                 return False
-        elif (num_byte >= 5 or num_byte == 1):
-            return False
-        return True
-    return False
+    return True
