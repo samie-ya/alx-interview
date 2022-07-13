1469d7ea30bb3419b53f1ce16001c109192d1132
Samie <samrasolomon40@gmail.com>
2022-07-13 15:19:32 +0000

    Made break correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@
 
 def validUTF8(data):
     """This script will return true if given list is a valid UTF-8"""
-    num_byte = 1
+    num_byte = 0
     bits = format(data[0], '08b')
     for bit in bits:
         if bit[0] == '0':
@@ -12,12 +12,11 @@ def validUTF8(data):
         else:
             if bit != '0':
                 num_byte += 1
-            break
-    if (num_byte > 1 and num_byte < 5):
+            else:
+                break
+    if (num_byte >= 2 and num_byte <= 4):
         for i in range(1, num_byte):
             bits = format(data[i], '08b')
             if (bits[0] != '1' and bits[1] != '0'):
                 return False
-    elif (num_byte >= 5):
-        return False
     return True
