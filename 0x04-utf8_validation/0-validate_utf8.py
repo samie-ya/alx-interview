172059de1f82f3d98cfa34dbd3c3292ed5eca9a5
Samie <samrasolomon40@gmail.com>
2022-07-13 17:12:01 +0000

    Made bit format modifications

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,18 +5,15 @@
 def validUTF8(data):
     """This script will return true if given list is a valid UTF-8"""
     num_byte = 0
-    bits = format(data[0], '08b')
+    bits = format(data[0], '#010b')[-8:]
     for bit in bits:
-        if bits[0] == '0':
+        if bit == '0':
             break
         else:
-            if bit != '0':
-                num_byte += 1
-            else:
-                break
+            num_byte += 1
     if (num_byte > 1 and num_byte < 5):
         for i in range(1, num_byte):
-            bits = format(data[i], '08b')
+            bits = format(data[i], '#010b')[-8:]
             if (bits[0] != '1' and bits[1] != '0'):
                 return False
     elif (num_byte >= 5 or num_byte == 1):
