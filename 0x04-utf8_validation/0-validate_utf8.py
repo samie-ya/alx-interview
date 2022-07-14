b0576e48ae4b7dc535a021c92023079babd3fc24
Samie <samrasolomon40@gmail.com>
2022-07-14 13:41:59 +0000

    Corrected the whole code to search iterate the dat

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,20 +5,28 @@
 def validUTF8(data):
     """This script will return true if given list is a valid UTF-8"""
     num_bytes = 0
-    bits = format(data[0], '#010b')[-8:]
-    for bit in bits:
-        if bit == '0':
-            break
+    for i in range(len(data)):
+        bits = format(data[i], '08b')
+        if bits[0] == '0':
+            continue
         else:
-            num_bytes += 1
-    if num_bytes == 1 or num_bytes > 4:
-        return False
-    if num_bytes > 1 and num_bytes <= 4:
-        for i in range(1, num_bytes):
-            new_bits = format(data[i], '#010b')[-8:]
-            if not (new_bits[0] == '1' and new_bits[1] == '0'):
+            for bit in bits:
+                if bit != '0':
+                    num_bytes += 1
+                else:
+                    break
+            if num_bytes == 1 or num_bytes >= 5:
                 return False
-            else:
-                continue
-        return True
+            if (num_bytes > 1 and num_bytes <= 4):
+                after_byte = i + 1
+                last_byte = i + num_bytes
+                if (after_byte > len(data) or last_byte > len(data)):
+                    return False
+                for j in range(after_byte, last_byte):
+                    new_bits = format(data[j], '08b')
+                    if not (new_bits[0] == '1' and new_bits[1] == '0'):
+                        return False
+                    else:
+                        continue
+                return True
     return True
