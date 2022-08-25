01f6f0ec42e7f464c7a299d76a52cc947179afff
Samie <samrasolomon40@gmail.com>
2022-08-25 09:00:23 +0000

    Took a new approach

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,43 +2,26 @@
 """This script will create a prime game"""
 
 
-def findPrimes(number):
-    """This function will find a list of prime numbers until number"""
-    primes = []
-    for num in range(number + 1):
-        if num > 1:
-            for i in range(2, num):
-                if (num % i == 0):
-                    break
-            else:
-                primes.append(num)
-    return primes
-
-
 def isWinner(x, nums):
     """This function will let Maria and Ben play"""
     Maria = 0
     Ben = 0
-    for num in nums:
+    for number in nums:
         maria = 0
         ben = 0
-        allNum = []
-        primeList = findPrimes(num)
-        for i in range(1, num + 1):
-            allNum.append(i)
-        if (primeList == []):
+        if (number == 1):
             ben += 1
         else:
-            for j in primeList:
-                if (maria == ben):
-                    maria += 1
-                elif (maria > ben):
-                    ben += 1
-                else:
-                    maria += 1
-                for i in allNum:
-                    if (i % j == 0):
-                        allNum.remove(i)
+            for num in range(number + 1):
+                if num > 1:
+                    for i in range(2, num):
+                        if (num % i == 0):
+                            break
+                    else:
+                        if maria == ben:
+                            maria += 1
+                        elif maria > ben:
+                            ben += 1
         if (maria > ben):
             Maria += 1
         elif (ben > maria):
