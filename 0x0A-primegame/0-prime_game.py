5ce929ca70798decc8cffa99d84c9f7e3dbf2541
Samie <samrasolomon40@gmail.com>
2022-08-25 08:53:45 +0000

    Added the wrong file previously

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script will create a prime game"""
 
+
 def findPrimes(number):
     """This function will find a list of prime numbers until number"""
     primes = []
@@ -18,22 +19,35 @@ def isWinner(x, nums):
     """This function will let Maria and Ben play"""
     Maria = 0
     Ben = 0
-    for number in nums:
+    for num in nums:
         maria = 0
         ben = 0
-        if (number == 1):
+        allNum = []
+        primeList = findPrimes(num)
+        for i in range(1, num + 1):
+            allNum.append(i)
+        if (primeList == []):
             ben += 1
         else:
-            for num in range(number + 1):
-                if num > 1:
-                    for i in range(2, num):
-                        if (num % i == 0):
-                            break
-                    else:
-                        if maria == ben:
-                            maria += 1
-                        elif maria > ben:
-                            ben += 1
-                        else:
-
-                    
+            for j in primeList:
+                if (maria == ben):
+                    maria += 1
+                elif (maria > ben):
+                    ben += 1
+                else:
+                    ben += 1
+                for i in allNum:
+                    if (i % j == 0):
+                        allNum.remove(i)
+        if (maria > ben):
+            Maria += 1
+        elif (ben > maria):
+            Ben += 1
+        else:
+            Ben += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    else:
+        return None
